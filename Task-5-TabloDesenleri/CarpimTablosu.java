e5777be8dc4e518f97c87b0796eba57c447961e0
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 18:59:59 +0300

    Implement multiplication table for 2

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,27 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        // Önce tablo başlığını ayarlayalım
+        System.out.println(" 2'nin Carpim Tablosu");
+        System.out.println("----------------------");
+        int carpim = 2*1;
+        System.out.println("2*1="+carpim);
+        int carpim2 = 2*2;
+        System.out.println("2*2="+carpim2);
+        int carpim3 = 2*3;
+        System.out.println("2*3="+carpim3);
+        int carpim4 = 2*4;
+        System.out.println("2*4="+carpim4);
+        int carpim5 = 2*5;
+        System.out.println("2*5="+carpim5);
+        int carpim6 = 2*6;
+        System.out.println("2*6="+carpim6);
+        int carpim7 = 2*7;
+        System.out.println("2*7="+carpim7);
+        int carpim8 = 2*8;
+        System.out.println("2*8="+carpim8);
+        int carpim9 = 2*9;
+        System.out.println("2*9="+carpim9);
+        int carpim10 = 2*10;
+        System.out.println("2*10="+carpim10);
+    }
+}
