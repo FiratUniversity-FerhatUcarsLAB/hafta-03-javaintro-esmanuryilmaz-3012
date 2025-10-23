284d2ab76a0c22723ad2e2c4c9f98387c84ef81f
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 18:04:17 +0300

    Add AsciiArtHarf class for ASCII art display

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println("eeeeeeeeeeee");
+        System.out.println("eeeeeeeeeeee");
+        System.out.println("eee         ");
+        System.out.println("eee         ");
+        System.out.println("eeeeeeeeee  ");
+        System.out.println("eeeeeeeeee  ");
+        System.out.println("eee         ");
+        System.out.println("eee         ");
+        System.out.println("eeeeeeeeeeee");
+        System.out.println("eeeeeeeeeeee");
+    }
+}
