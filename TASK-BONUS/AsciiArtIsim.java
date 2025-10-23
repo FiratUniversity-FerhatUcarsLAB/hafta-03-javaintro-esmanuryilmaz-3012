856fe71cdfd1ec77b731773c98bed3f0069169d5
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 20:22:31 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,14 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("eeeeeeeeeeee         sssssssssss       mmmmmm              mmmmmm          aaaa          ");
+        System.out.println("eeeeeeeeeeee        ssssssssssss       mmmmmmm            mmmmmmm         aaaaaa         ");
+        System.out.println("eee                sssss               mmmm  mmmmm    mmmmm  mmmm        aaa  aaa        ");
+        System.out.println("eee                 sssss              mmmm    mmmm  mmmm    mmmm       aaa    aaa       ");
+        System.out.println("eeeeeeeeee            sssss            mmmm       mmmmm      mmmm      aaaa    aaaa      ");
+        System.out.println("eeeeeeeeee               sssss         mmmm        mmm       mmmm     aaaa      aaaa     ");
+        System.out.println("eee                        sssss       mmmm                  mmmm    aaaaaaaaaaaaaaaa    ");
+        System.out.println("eee                         sssss      mmmm                  mmmm    aaaa        aaaa    ");
+        System.out.println("eeeeeeeeeeee        ssssssssssss       mmmm                  mmmm   aaaaa        aaaaa   ");
+        System.out.println("eeeeeeeeeeee         sssssssssss       mmmm                  mmmm  aaaaaa        aaaaaa  ");
+    }
+}
