d63c09bab32b4995759f94fa1c90b473d1f34b44
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 10:46:18 +0300

    Add DaireHesap class for circle calculations

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,18 @@
+public class DaireHesap {
+    public static void main (String [] args){
+        double r =5.5;
+        double pi= 3.14;
+
+        // Alan hesapla
+        double alan = pi*r*r;
+
+        // Cevre hesapla
+        double cevre = 2*pi*r;
+
+        // Sonucları yazdır
+        System.out.println("Yaricap:" + r);
+        System.out.println("Alan:" + alan);
+        System.out.println("Cevre:" + cevre);
+
+    }
+}
