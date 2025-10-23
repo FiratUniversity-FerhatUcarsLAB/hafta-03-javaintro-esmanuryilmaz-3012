dc24c7a0855b7fa94c1fc1f9177b8bd6c147dc0f
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 11:25:35 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        // Gerekli bilgileri gir
+        String ad ="Esmanur";
+        String soyad="Yılmaz";
+        String ogrNo= "250541033";
+        String bolum= "Yazilim Muhendisligi";
+
+        // Yazdır
+
+        System.out.println("========================");
+        System.out.println(" OGRENCİ BILGILERI ");
+        System.out.println("========================");
+        System.out.println("Adı        :" + ad);
+        System.out.println("Soyadı      :" + soyad);
+        System.out.println("Ogrenci No :" + ogrNo);
+        System.out.println("Bolumu     :" + bolum);
+        System.out.println("========================");
+    }
+}
