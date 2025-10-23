a8475db1592c4dc823e8eced9b47fc54dd5d460b
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 10:32:48 +0300

    Add factorial calculation in FaktoriyelHesaplama class

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,8 @@
+public class FaktoriyelHesaplama {
+    public static void main(String[] args) {
+        // Faktoriyeli hesapla
+      int faktoriyel= 5*4*3*2*1;
+      // Sonucu ekrana yazdır
+      System.out.println("5!=5*4*3*2*1=" + faktoriyel);
+    }
+}
