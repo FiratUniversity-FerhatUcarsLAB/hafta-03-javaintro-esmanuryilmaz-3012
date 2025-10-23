de8ad23e13697ad3039f7bb19ca21092e81eab44
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 10:34:01 +0300

    Update ToplamaSeri.java

--- Task-2-Matematikselislemler/ToplamaSeri.java
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -1,4 +1,4 @@
-public class ToplamaSerisi {
+public class ToplamaSeri {
     public static void main(String[] args) {
         // Sonucu hesapla
         int sonuc = 1+2+3+4+5+6+7+8+9;
@@ -6,3 +6,4 @@ public class ToplamaSerisi {
         System.out.println("1+2+3+4+5+6+7+8+9=" + sonuc);
     }
 }
+
