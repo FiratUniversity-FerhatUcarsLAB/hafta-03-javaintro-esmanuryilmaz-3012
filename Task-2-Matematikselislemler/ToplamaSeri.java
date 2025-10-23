d84f5e310e70b2ed11e04c596fa0eb012bc0af99
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 10:34:26 +0300

    Rename class ToplamaSeri to ToplamaSerisi

--- Task-2-Matematikselislemler/ToplamaSeri.java
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -1,4 +1,4 @@
-public class ToplamaSeri {
+public class ToplamaSerisi {
     public static void main(String[] args) {
         // Sonucu hesapla
         int sonuc = 1+2+3+4+5+6+7+8+9;
@@ -6,4 +6,3 @@ public class ToplamaSeri {
         System.out.println("1+2+3+4+5+6+7+8+9=" + sonuc);
     }
 }
-
