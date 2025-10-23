e251dc27b90eb6e9121ec014aea0928950d46892
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-10-23 10:58:47 +0300

    Implement rectangle area and perimeter calculations

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,17 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        // En ve boy bilgileri gir
+        double en = 4.5;
+        double boy = 7.9;
+
+        // Cevre hesapla
+        double cevre = 4.5*2+7.9*2;
+
+        // Alan hesapla
+        double alan = 4.5*7.9;
+
+        // Ekrana yazdır
+        System.out.println("Alan:" + alan);
+        System.out.println("Çevre:" + cevre);
+    }
+}
